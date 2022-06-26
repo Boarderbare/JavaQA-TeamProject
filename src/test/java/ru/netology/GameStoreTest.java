951c83c8cbf709fb9a15e969cebde4f0230f4d98
boarderbare <boarderbare@gmail.com>
2022-06-26 17:32:18 +0400

    add tests for Gamestore

--- src/test/java/ru/netology/GameStoreTest.java
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -1,18 +1,86 @@
 package ru.netology;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameStoreTest {
 
+    private GameStore store = new GameStore();
+
+//  наверно можно удалить так как он дублируется
+//    @Test
+//    public void shouldAddGame() {
+//
+//        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+//        assertTrue(store.containsGame(game));
+//    }
+
     @Test
-    public void shouldAddGame() {
-
-        GameStore store = new GameStore();
+    public void shouldContainsGame() {
         Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
-
         assertTrue(store.containsGame(game));
     }
 
-    // другие ваши тесты
+    @Test
+    public void shouldNotContainsGame() {
+        GameStore store1 = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game1 = new Game("GTA V", "Экшен",store1);
+        assertFalse(store.containsGame(game1));
+    }
+
+    @Test
+    public void shouldAddPlayTime() {
+
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        game.getStore().addPlayTime("Vasya", 5);
+        game.getStore().addPlayTime("Petya", 4);
+
+        String expected = "Vasya";
+        String actual = store.getMostPlayer();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldAddPlayTimeSum() {
+
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        store.addPlayTime("Vasya", 3);
+        store.addPlayTime("Petya", 4);
+        store.addPlayTime("Vasya", 5);
+        String expected = "Vasya";
+        String actual = store.getMostPlayer();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldGetMostPlayer() {
+
+        store.addPlayTime("Vasya", 3);
+        store.addPlayTime("Petya", 4);
+        store.addPlayTime("Vasya", 5);
+        String expected = "Vasya";
+        String actual = store.getMostPlayer();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNoGetMostPlayer() {
+        String actual = store.getMostPlayer();
+        assertNull(actual);
+    }
+
+    @Test
+    void getSumPlayedTime() {
+        store.addPlayTime("Vasya", 3);
+        store.addPlayTime("Petya", 0);
+        store.addPlayTime("Vanya", 20);
+        int actual = store.getSumPlayedTime();
+        int expected = 23;
+        assertEquals(expected, actual);
+    }
 }
